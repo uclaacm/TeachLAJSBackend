fc526f03d6a6736d7b694be98bac98c4df091861
Connor Borden <connorbo97@gmail.com>
2019-04-26 22:23:44 -0700

    reset to prod db for release

--- firebase/index.js
+++ firebase/index.js
@@ -3,12 +3,13 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 var config;
+config = require("../firebase-config-prod.json");
 
-if (process && process.env && process.env.FS_DB === "prod") {
-  config = require("../firebase-config-prod.json");
-} else {
-  config = require("../firebase-config.json");
-}
+// if (process && process.env && process.env.FS_DB === "prod") {
+//   config = require("../firebase-config-prod.json");
+// } else {
+//   config = require("../firebase-config.json");
+// }
 
 firebase.initializeApp(config);
 
